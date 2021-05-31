5d5bf587eaa4f6b04c4b7ad947ed9dd7248ca6b3
krishna0379 <iamkrishna0379@gmail.com>
2021-05-31 20:47:35 +0530

    mini insta vth random images

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import "./App.css";
 import { useState } from "react";
 
 export default function App() {
-  const [list] = useState([1, 2, 3, 4, 5, 6,7,8,9]);
+  const [list] = useState([1, 1, 1, 1, 1]);
   return (
-    <div className="bg-secondary ">
-      <h1 className="bg-info p-3 rounded  sticky-top ">React with Bootstrap</h1>
+    <div className="alert-info">
+      <h1 className="bg-danger  text-info p-3 rounded sticky-top"> Mini Instagram</h1>
+
       {list.map((item, index) => (
-        <div key={index} className=" alert-info p-3 m-3 rounded">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          consequatur! Libero corporis sit reiciendis minus, modi molestiae
-          laborum iste sed illum eveniet error voluptas rem eligendi quasi
-          inventore non aut. Libero accusantium quas sint illo.
+        <div
+          key={index}
+          className=" m-3 rounded"
+          style={{ height: "300px" }}
+        >
+          <img
+            src= {"https://picsum.photos/300" + index}
+            className="rounded"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            alt=""
+          />
         </div>
       ))}
     </div>
