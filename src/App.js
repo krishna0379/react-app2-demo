16a52420dd63b70c4552202c86d8bef054efe63b
krishna0379 <iamkrishna0379@gmail.com>
2021-05-31 21:25:20 +0530

    delete option added

--- src/App.js
+++ src/App.js
@@ -12,26 +12,43 @@ export default function App() {
     setList([textInput, ...list]);
     setTextInput("");
   };
-  return(
-  <div className="bg-dark ">
-    <h1 className="bg-info p-3">Mini Twitter</h1>
-    <div className="m-3 mb-4">
-      <input
-        type="text"
-        value={textInput}
-        onChange={(e) => updateTextInput(e)}
-        className="from-control mb-2"
-        placeholder="post Tweet"
-      />
-      <input 
-      type="button"
-       className="btn-info"
-       onClick={()=> postTweet()}
-       value="Tweet" />
+
+  const deleteTweet = (item, index) => {
+    //which twwet has to delete..
+    list.splice(index, 1);
+
+    //swe can update the view
+    setList([...list]);
+  };
+  return (
+    <div className="bg-dark ">
+      <h1 className="bg-info p-3">Mini Twitter</h1>
+      <div className="m-3 mb-4">
+        <input
+          type="text"
+          value={textInput}
+          onChange={(e) => updateTextInput(e)}
+          className="from-control mb-2"
+          placeholder="post Tweet"
+        />
+        <input
+          type="button"
+          className="btn-info"
+          onClick={() => postTweet()}
+          value="Tweet"
+        />
+      </div>
+      {list.map((item, index) => (
+        <div key={index} className="alert-secondary">
+          {item}
+          <button
+            className="btn btn-outline-danger m-3"
+            onClick={() => deleteTweet(item, index)}
+          >
+            X
+          </button>
+        </div>
+      ))}
     </div>
-    {list.map((item, index) =>(
-    <div key={index} className="alert-secondary">{item}</div>
-    ))}
-  </div>
   );
 }
